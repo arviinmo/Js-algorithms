d970ffcea0bb0a8da16fa1bb17615855227bebc1
Arvîn mostafaei <Arvinmostafaeii@gmail.com>
2022-08-05 01:12:30 +0430

    add four sulotion for factorial algorithm

---
+++ algorithms/factorial.js
@@ -0,0 +1,34 @@
+//first one
+function factorialize(num) {
+  if (num === 0) {
+    return 1;
+  }
+  return num * factorialize(num - 1);
+}
+
+
+//second one
+function factorialize(num) {
+  let product = 1;
+  for (let i = 2; i <= num; i++) {
+    product *= i;
+  }
+  return product;
+}
+
+//third one
+function factorialize(num) {
+  return num < 0 ? 1 :
+    new Array(num)
+      .fill(undefined)
+      .reduce((product, _, index) => product * (index + 1), 1);
+}
+
+//forth one
+function factorialize(num, factorial = 1) {
+  if (num === 0) {
+    return factorial;
+  } else {
+    return factorialize(num - 1, factorial * num);
+  }
+}
