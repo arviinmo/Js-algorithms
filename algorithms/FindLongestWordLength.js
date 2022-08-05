adaa16e77b571a9898b1626ba2b5826c7a7be7c5
Arvin mostafaei <70845998+arviinmo@users.noreply.github.com>
2022-08-05 16:06:48 +0430

    add two more solution

--- algorithms/FindLongestWordLength.js
+++ algorithms/FindLongestWordLength.js
@@ -15,3 +15,23 @@ function findLongestWordLength(str) {
 function findLongestWordLength(str) {
   return Math.max(...str.split(" ").map((word) => word.length));
 }
+
+//third one
+function findLongestWordLength(str) {
+  let words = str.split(" ");
+  let maxLength = 0;
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > maxLength) {
+      maxLength = words[i].length;
+    }
+  }
+  return maxLength;
+}
+
+//last one using .reduce()
+function findLongestWordLength(str) {
+  return str.split(" ").reduce(function (longest, word) {
+    return Math.max(longest, word.length);
+  }, 0);
+}
