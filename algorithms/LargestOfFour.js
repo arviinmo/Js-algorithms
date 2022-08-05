7c7f412481cf21ba75ea12f9e27f6af54c642ca9
Arvin mostafaei <70845998+arviinmo@users.noreply.github.com>
2022-08-05 18:34:42 +0430

    Largest Of Four Numbers

---
+++ algorithms/LargestOfFour.js
@@ -0,0 +1,26 @@
+// Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
+// Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
+
+// first solution
+function largestOfFour(arr) {
+  const results = [];
+  for (let i = 0; i < arr.length; i++) {
+    let largestNumber = arr[i][0];
+    for (let j = 1; j < arr[i].length; j++) {
+      if (arr[i][j] > largestNumber) {
+        largestNumber = arr[i][j];
+      }
+    }
+    results[i] = largestNumber;
+  }
+  return results;
+}
+
+// second solution (Declarative approach)
+function largestOfFour(arr) {
+  return arr.map(function (group) {
+    return group.reduce(function (prev, current) {
+      return current > prev ? current : prev;
+    });
+  });
+}
