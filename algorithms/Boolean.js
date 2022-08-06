1373c5b21cfa7ec0ac9477e828579ee271ab2d3a
Arvin mostafaei <70845998+arviinmo@users.noreply.github.com>
2022-08-06 11:46:42 +0430

    Boo Who algo

---
+++ algorithms/Boolean.js
@@ -0,0 +1,7 @@
+// Check if a value is classified as a boolean primitive. Return true or false.
+// Boolean primitives are true and false.
+
+//using typeof
+function booWho(bool) {
+  return typeof bool === "boolean";
+}
