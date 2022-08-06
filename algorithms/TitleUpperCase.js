646f60b851693abac16d82fe01d5744220c6dd6e
Arvin mostafaei <70845998+arviinmo@users.noreply.github.com>
2022-08-06 12:29:01 +0430

    Title To UpperCase
    convert Title of a given text to Uppercase!

--- algorithms/TitleUpperCase.js
+++ algorithms/TitleUpperCase.js
@@ -6,3 +6,22 @@ function titleCase(str) {
   return str.toLowerCase().replace(/(^|\s)\S/g, (L) => L.toUpperCase());
 }
 
+//second solution
+function titleCase(str) {
+  const newTitle = str.split(" ");
+  const updatedTitle = [];
+  for (let st in newTitle) {
+    updatedTitle[st] =
+      newTitle[st][0].toUpperCase() + newTitle[st].slice(1).toLowerCase();
+  }
+  return updatedTitle.join(" ");
+}
+
+//third one
+function titleCase(str) {
+  return str
+    .toLowerCase()
+    .split(" ")
+    .map((val) => val.replace(val.charAt(0), val.charAt(0).toUpperCase()))
+    .join(" ");
+}
