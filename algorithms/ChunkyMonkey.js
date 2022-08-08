86ed901cead6c51e2684019e1397ed5a75ce1373
Arvin mostafaei <70845998+arviinmo@users.noreply.github.com>
2022-08-09 00:22:21 +0430

    chunky monkey algorithm

--- algorithms/ChunkyMonkey.js
+++ algorithms/ChunkyMonkey.js
@@ -38,3 +38,21 @@ function chunkArrayInGroups(arr, size) {
   if (temp.length !== 0) result.push(temp);
   return result;
 }
+
+// 4
+function chunkArrayInGroups(arr, size) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += size) {
+    newArr.push(arr.slice(i, i + size));
+  }
+  return newArr;
+}
+
+// 5
+function chunkArrayInGroups(arr, size) {
+  let newArr = [];
+  while (arr.length > 0) {
+    newArr.push(arr.splice(0, size));
+  }
+  return newArr;
+}
