3c63895854b8ab428e37bc5fc2ec5bf59f0c8310
Arvin mostafaei <70845998+arviinmo@users.noreply.github.com>
2022-08-15 11:19:59 +0430

    IIFE

---
+++ Data Structure/ImmediatelyInvokedFunctionExpression(IIFE).js
@@ -0,0 +1,10 @@
+// A common pattern in JavaScript is to execute a function as soon as it is declared:
+
+(function () {
+  console.log("Chirp, chirp!");
+})();
+// This is an anonymous function expression that executes right away, and outputs Chirp, chirp! immediately.
+// Note that the function has no name and is not stored in a variable. The two parentheses () at the end of the
+// function expression cause it to be immediately executed or invoked. This pattern is known as an immediately invoked function expression or IIFE.
+
+// Link = https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/object-oriented-programming/understand-the-immediately-invoked-function-expression-iife
