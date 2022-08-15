d774bb88c4a3e5349d0434c16933d9bed460e5c0
Arvin mostafaei <70845998+arviinmo@users.noreply.github.com>
2022-08-15 11:16:42 +0430

    Use Closure to Protect Properties Within an Object

--- Data Structure/UseClosuretoProtectPropertiesWithinanObject.js
+++ Data Structure/UseClosuretoProtectPropertiesWithinanObject.js
@@ -20,3 +20,5 @@ ducky.getHatchedEggCount();
 // Here getHatchedEggCount is a privileged method, because it has access to the private variable hatchedEgg.
 // This is possible because hatchedEgg is declared in the same context as getHatchedEggCount. In JavaScript,
 // a function always has access to the context in which it was created. This is called closure.
+
+// Link = https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/object-oriented-programming/use-closure-to-protect-properties-within-an-object-from-being-modified-externally
